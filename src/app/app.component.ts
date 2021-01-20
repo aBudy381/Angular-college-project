ca99e95192b4a52f209dcdba64d3439b509fc528
aBody381 <assala.benahmed98@gmail.com>
2021-01-20 22:53:15 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {JSONPlaceholderService} from './services/jsonplaceholder.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+
+  data:Array<any>
+
+  constructor(private jsonplaceholder:JSONPlaceholderService){
+
+    this.data=new Array<any>()
+  }
+  getDataFromAPI(val: string) {
+    this.jsonplaceholder.getData().subscribe((data) => {
+      for (let i = 0; i < data.length; i++) {
+        Object.keys(data[i]).forEach((key) => {
+          if (data[i][key] === val) {
+            Object.keys(data[i]).forEach((key) => {
+              document.write(
+                data[i][key]
+              );
+            });
+            document.write('<br>');
+            document.write('<br>');
+            this.data.push(data[i]);
+          }
+        });
+      }
+      if (this.data.length === 0) {
+        document.write('user not found');
+      }
+      console.log(this.data);
+    });
+  }
+  
 }
